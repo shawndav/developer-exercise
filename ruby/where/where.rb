8fff194f3fa94b1982f2dfe1378c36225dac1937
Shawn Wilkinson <shawnwilkinson11@gmail.com>
2015-10-28 23:21:29 -0500

    passing 4 out of 5 tests for where

--- ruby/where/where.rb
+++ ruby/where/where.rb
@@ -1,8 +1,34 @@
 module Where
 
-  def where
-
+  def where(selectors)
+    matches = []
+    selectors.each do |selector|
+      self.each do |element|
+        key = selector[0]
+        desired_value = selector[1]
+        if (element[key] == desired_value) || (desired_value.is_a?(Regexp) && (element[key] =~ desired_value))
+          matches << element
+        end
+      end
+    end
+    return matches
   end
+
+  # def new_where(selectors)
+  #   matches = []
+  #     if selector.length == 1
+  #       self.each do |element|
+  #         key = selector[0]
+  #         desired_value = selector[1]
+  #         if (element[key] == desired_value) || (desired_value.is_a?(Regexp) && (element[key] =~ desired_value))
+  #           matches << element
+  #         end
+  #       end
+  #     else
+  #       matches =
+  #     end
+  #   return matches
+  # end
 
 end
 
@@ -11,3 +37,5 @@ end
 class Array
   include Where
 end
+
+
